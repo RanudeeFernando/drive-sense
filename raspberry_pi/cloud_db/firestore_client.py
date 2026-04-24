fc94ad8710aef2d3fb5c30454a389fed44769e10
Ranudee Fernando <164500480+RanudeeFernando@users.noreply.github.com>
2026-04-24 05:39:51 +0530

    code optimization - credentials common path fix

--- raspberry_pi/cloud_db/firestore_client.py
+++ raspberry_pi/cloud_db/firestore_client.py
@@ -15,7 +15,7 @@ def get_db():
     if _db is None:
         if not firebase_admin._apps:
             # Adjust path relative to raspberry_pi folder
-            base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
             cred_path = os.path.join(
                 base_dir,
